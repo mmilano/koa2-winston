f604b7807851cbd29faa7960f7e9ed5105af69c9
mmilano <mmilano+git@gmail.com>
2020-06-30 20:50:49 -0700

    update tests to work with ava3

--- test/logger.stringify.test.js
+++ test/logger.stringify.test.js
@@ -1,4 +1,5 @@
-import test from 'ava';
+// import test from 'ava';
+const test = require('ava');
 
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
--- test/stringify_schema.test.js
+++ test/stringify_schema.test.js
@@ -1,4 +1,5 @@
-import test from 'ava';
+// import test from 'ava';
+const test = require('ava');
 
 const { generateSchema } = require('../stringify_schema');
 
